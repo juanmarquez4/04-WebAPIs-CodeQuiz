46b7cae2d818e1f4ebe6ab890a3b009d9ab140c4
Juan Carlos Marquez <JuanCarlos@MacBook-Pro.local>
2022-06-12 15:07:17 +1000

    Updated js with correct answers for loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,46 @@
-// variables
+// declared variables
 var timeEl = document.getElementById("counter");
 var startButton = document.getElementById("startQuiz");
-var question = 
+var question1 =  [
     {
         title: "Commonly used data types DO NOT include:",
         alternatives: ["1.strings", "2.booleans", "3.alerts", "4.numbers"],
         correctAnswer: "1.strings",
-    };
-    // {
-    //     title: "The condition in an if / else statement is enclosed within ____.",
-    //     options: ["1.quotes", "2.curly brackets", "3.parenthesis", "4.square brackets"],
-    //     correctAnswer: 3
-    // },
-    // {
-    //     title: "Arrays in JavaScript can be used to store ____",
-    //     options: ["1.numbers and strings", "2.other arrays", "3.booleans", "4.all of the above"],
-    //     correctAnswer: 0
-    // },
-    // {
-    //     title: "String Values must be enclosed within ____ when being assigned to vasriables.",
-    //     options: ["1.commas", "2.curly brackets", "3.quotes", "4.parenthesis"],
-    //     correctAnswer: 0
-    // },
-    // {
-    //     title: "A very useful tool used during development and debugging for printing content to the debuger is:",
-    //     options: ["1.JavaScript", "2.terminal / bash", "3.for loops", "4.console.log"],
-    //     correctAnswer: 0
-    // },
+    },
+// var question2 =
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        alternatives: ["1.quotes", "2.curly brackets", "3.parenthesis", "4.square brackets"],
+        correctAnswer: "3.parenthesis"
+    },
+// var question3 =
+    {
+        title: "Arrays in JavaScript can be used to store ____",
+        alternatives: ["1.numbers and strings", "2.other arrays", "3.booleans", "4.all of the above"],
+        correctAnswer: "4.all of the above"
+    },
+// var question4 =
+    {
+        title: "String Values must be enclosed within ____ when being assigned to variables.",
+        alternatives: ["1.commas", "2.curly brackets", "3.quotes", "4.parenthesis"],
+        correctAnswer: "3.quotes"
+    },
+// var question5 =
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debuger is:",
+        alternatives: ["1.JavaScript", "2.terminal / bash", "3.for loops", "4.console.log"],
+        correctAnswer: "4.console.log"
+    },
+
+]
+    
 
 
         
 
 // start quiz button
 
-startButton.addEventListener("click", swapDivsWithClick);
+startButton.addEventListener("click", swapDivsWithClick); 
 startButton.addEventListener("click", startGame);
 
 // timer function
@@ -53,8 +60,6 @@ function startGame() {
     }, 1000);
 }
 
-
-
 // display and hide card containers
 function swapDivsWithClick(){
     var d1 = document.getElementById("card");
@@ -71,34 +76,51 @@ function swapDivsWithClick(){
 
 
 // show multiple questions
-function showQuestion(q) {
-    var titleDiv = document.getElementById("title");
-    titleDiv.textContent = q.title;
 
-    var alternativesDiv = document.querySelectorAll(".alternatives");
-    for (var i=0; i < alternativesDiv.length; i++){
-        alternativesDiv[i].textContent = q.alternatives[i];
-    }
+for( var i=0; i<question1.length;i++){
     
-    // btn0.addEventListener("click", function(){
-    //     if(q.correctAnswer == q.alternatives){
-    //         console.log("correct answer")
-    //     }
-    //     else{
-    //         console.log("Wrong answer")
-    //     }
-    // })
+    function showQuestion(q) {
+        var titleDiv = document.getElementById("title");
+        titleDiv.textContent = q.title;
+    
+        var alternativesDiv = document.querySelectorAll(".alternatives");
+        for (var i=0; i < alternativesDiv.length; i++){
+            if(
+                q.alternatives[i] == q.correctAnswer
+            )
+            {
+                alternativesDiv[i].textContent = q.alternatives[i];
+                alternativesDiv[i].setAttribute("data-iscorrect","true")
+            }else{
+                alternativesDiv[i].textContent = q.alternatives[i];
+                alternativesDiv[i].setAttribute("data-iscorrect","false")
+            }
+            
+        };
+        
+        };
+       
+    
+    var alternativesDiv = document.querySelectorAll(".alternatives");
+    for (let i=0; i<alternativesDiv.length;i++){
+        alternativesDiv[i].addEventListener("click",function(){
+          if (this.dataset.iscorrect == "true"){
+            startButton.textContent = "Correct"
+          }else{
+            startButton.textContent = "Incorrect"
+          }
+    
+        })
+    }; 
+    
+    showQuestion(question1);
+    
+}
 
-    };
-   
 
-
-showQuestion(question);
 
 
 //if answer is wrong, time is taken off the timer
-
-
 
 //when all questions are answered, or timer gets to 0, then game is over.
 
@@ -111,3 +133,38 @@ showQuestion(question);
 
     // alts.forEach(function(element, index){
     //     element.textContent = q.alternatives[index]; 
+
+
+    // console.log(alternativesDiv);
+    // var e = document.getElementById("btn0");
+    // console.log(e.textContent)
+    // if(e.textContent == q.correctAnswer){
+    //     console.log("Right Answer")
+    // }else{
+    //     console.log("Wrong Answer")
+    // };
+    
+ 
+
+    
+    
+    // var buttons = document.querySelectorAll(".alternatives");
+    // console.log(buttons);
+    
+    // buttons.addEventListener("click", function(event){
+    //    buttons = event.target.innerHTML;
+
+
+    // })
+
+    // .addEventListener("click", function(event){
+    //     var rightanswer = event.target.textContent;
+    //     console.log(rightanswer)
+
+    //     // if(q.correctAnswer == q.alternatives[i]){
+    //     //     console.log("correct answer")
+    //     // }
+    //     // else{
+    //     //     console.log("Wrong answer")
+    //     // }
+    // })
