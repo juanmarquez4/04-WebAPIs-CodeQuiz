61ec13c2aa49ac3d2f5adbdd3d0c40e4ac63c080
Juan Carlos Marquez <JuanCarlos@MacBook-Pro.local>
2022-06-12 15:28:35 +1000

    updated js with question fuctionn iterartion issue

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,36 @@
 // declared variables
 var timeEl = document.getElementById("counter");
 var startButton = document.getElementById("startQuiz");
-var question1 =  [
+var question1 =  
     {
         title: "Commonly used data types DO NOT include:",
         alternatives: ["1.strings", "2.booleans", "3.alerts", "4.numbers"],
         correctAnswer: "1.strings",
-    },
-// var question2 =
+    };
+var question2 =
     {
         title: "The condition in an if / else statement is enclosed within ____.",
         alternatives: ["1.quotes", "2.curly brackets", "3.parenthesis", "4.square brackets"],
         correctAnswer: "3.parenthesis"
-    },
-// var question3 =
+    };
+var question3 =
     {
         title: "Arrays in JavaScript can be used to store ____",
         alternatives: ["1.numbers and strings", "2.other arrays", "3.booleans", "4.all of the above"],
         correctAnswer: "4.all of the above"
-    },
-// var question4 =
+    };
+var question4 =
     {
         title: "String Values must be enclosed within ____ when being assigned to variables.",
         alternatives: ["1.commas", "2.curly brackets", "3.quotes", "4.parenthesis"],
         correctAnswer: "3.quotes"
-    },
-// var question5 =
+    };
+var question5 =
     {
         title: "A very useful tool used during development and debugging for printing content to the debuger is:",
         alternatives: ["1.JavaScript", "2.terminal / bash", "3.for loops", "4.console.log"],
         correctAnswer: "4.console.log"
-    },
-
-]
-    
+    };
 
 
         
@@ -53,6 +50,8 @@ function startGame() {
 
         if(secondsLeft === 0) {
             clearInterval(timerInterval);
+            timeEl.textContent = "Game is Over!!!"
+            allDone()
         }
 
     startButton.disabled = true;
@@ -68,57 +67,52 @@ function swapDivsWithClick(){
       d1.style.display = "none";
       d2.style.display = "block";
    } 
-// else {
-//       d1.style.display = "block";
-//       d2.style.display = "none";
-//    }
 };
 
 
 // show multiple questions
 
-for( var i=0; i<question1.length;i++){
-    
-    function showQuestion(q) {
-        var titleDiv = document.getElementById("title");
-        titleDiv.textContent = q.title;
-    
-        var alternativesDiv = document.querySelectorAll(".alternatives");
-        for (var i=0; i < alternativesDiv.length; i++){
-            if(
-                q.alternatives[i] == q.correctAnswer
-            )
-            {
-                alternativesDiv[i].textContent = q.alternatives[i];
-                alternativesDiv[i].setAttribute("data-iscorrect","true")
-            }else{
-                alternativesDiv[i].textContent = q.alternatives[i];
-                alternativesDiv[i].setAttribute("data-iscorrect","false")
-            }
-            
-        };
-        
-        };
-       
-    
+function showQuestion(q) {
+    var titleDiv = document.getElementById("title");
+    titleDiv.textContent = q.title;
+
     var alternativesDiv = document.querySelectorAll(".alternatives");
-    for (let i=0; i<alternativesDiv.length;i++){
-        alternativesDiv[i].addEventListener("click",function(){
-          if (this.dataset.iscorrect == "true"){
-            startButton.textContent = "Correct"
-          }else{
-            startButton.textContent = "Incorrect"
-          }
+    for (var i=0; i < alternativesDiv.length; i++){
+        if(
+            q.alternatives[i] == q.correctAnswer
+        )
+        {
+            alternativesDiv[i].textContent = q.alternatives[i];
+            alternativesDiv[i].setAttribute("data-iscorrect","true")
+        }else{
+            alternativesDiv[i].textContent = q.alternatives[i];
+            alternativesDiv[i].setAttribute("data-iscorrect","false")
+        }
+        
+    };
     
-        })
-    }; 
-    
-    showQuestion(question1);
-    
-}
+    };
+   
+
+var alternativesDiv = document.querySelectorAll(".alternatives");
+for (let i=0; i<alternativesDiv.length;i++){
+    alternativesDiv[i].addEventListener("click",function(){
+      if (this.dataset.iscorrect == "true"){
+        startButton.textContent = "Correct"
+      }else{
+        startButton.textContent = "Incorrect"
+      }
+
+    })
+}; 
+
+showQuestion(question1);
 
 
+// run this function when timer gets to 0 seconds
+function allDone(){
 
+};
 
 //if answer is wrong, time is taken off the timer
 
@@ -168,3 +162,8 @@ for( var i=0; i<question1.length;i++){
     //     //     console.log("Wrong answer")
     //     // }
     // })
+
+    // else {
+//       d1.style.display = "block";
+//       d2.style.display = "none";
+//    }
